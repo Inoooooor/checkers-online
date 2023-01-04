dbd6aaad361f471ac02792f2718e6707c97079eb
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-01-04 22:06:40 +0300

    added_proto_queen_hints

--- tailwind_vue/src/components/Game.vue
+++ tailwind_vue/src/components/Game.vue
@@ -32,14 +32,15 @@
             class=""
             v-if="x.isBlack && x.isQueen"
           >
-            <img src="../images/queen_figure_black.png" alt="Black Queen"></div>
+            <img src="../images/queen_figure_black.png" alt="Black Queen" />
+          </div>
           <div
             @click="chosenCheckerHint(y, x)"
             id="white_queen_checker"
             class=""
             v-if="x.isWhite && x.isQueen"
           >
-            <img src="../images/queen_figure_white.png" alt="White Queen">
+            <img src="../images/queen_figure_white.png" alt="White Queen" />
           </div>
           <div
             @click="moveChecker(y, x)"
@@ -67,23 +68,19 @@ export default {
   data() {
     return {
       isChosen: 1,
-      field: new Array(8)
-        .fill()
-        .map(() =>
-          new Array(8)
-            .fill()
-            .map((item, index) => ({
-              id: index,
-              isChosen: 0,
-              isChecker: 0,
-              isWhite: 0,
-              isBlack: 0,
-              isHinted: 0,
-              isPlayble: 0,
-              isEnemyNear: 0,
-              isQueen: 0,
-            }))
-        ),
+      field: new Array(8).fill().map(() =>
+        new Array(8).fill().map((item, index) => ({
+          id: index,
+          isChosen: 0,
+          isChecker: 0,
+          isWhite: 0,
+          isBlack: 0,
+          isHinted: 0,
+          isPlayble: 0,
+          isEnemyNear: 0,
+          isQueen: 0,
+        }))
+      ),
     };
   },
   methods: {
@@ -168,7 +165,7 @@ export default {
       for (let row in this.field) {
         for (let col in this.field[row]) {
           // if (this.field[row][col].isHinted == 1) {
-            this.field[row][col].isHinted = 0;
+          this.field[row][col].isHinted = 0;
           // }
         }
       }
@@ -177,7 +174,7 @@ export default {
       for (let row in this.field) {
         for (let col in this.field[row]) {
           // if (this.field[row][col].isChosen == 1) {
-            this.field[row][col].isChosen = 0;
+          this.field[row][col].isChosen = 0;
           // }
         }
       }
@@ -210,7 +207,10 @@ export default {
             this.field[i + 1][j + 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i + 1][j + 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i + 1][j + 1]
+            );
             this.field[i + 1][j + 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -224,7 +224,10 @@ export default {
             this.field[i + 1][j - 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i + 1][j - 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i + 1][j - 1]
+            );
             this.field[i + 1][j - 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -238,7 +241,10 @@ export default {
             this.field[i - 1][j + 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i - 1][j + 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i - 1][j + 1]
+            );
             this.field[i - 1][j + 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -252,7 +258,10 @@ export default {
             this.field[i - 1][j - 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i - 1][j - 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i - 1][j - 1]
+            );
             this.field[i - 1][j - 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -267,7 +276,10 @@ export default {
           ) {
             // let objBuffer;
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i + 1][j + 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i + 1][j + 1]
+            );
             this.field[i + 1][j + 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -281,7 +293,10 @@ export default {
             this.field[i + 1][j - 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i + 1][j - 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i + 1][j - 1]
+            );
             this.field[i + 1][j - 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -295,7 +310,10 @@ export default {
             this.field[i - 1][j + 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i - 1][j + 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i - 1][j + 1]
+            );
             this.field[i - 1][j + 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
@@ -309,15 +327,17 @@ export default {
             this.field[i - 1][j - 1].isChosen
           ) {
             objBuffer = Object.assign({}, this.field[y_axis][x_axis]);
-            this.field[y_axis][x_axis] = Object.assign({}, this.field[i - 1][j - 1]);
+            this.field[y_axis][x_axis] = Object.assign(
+              {},
+              this.field[i - 1][j - 1]
+            );
             this.field[i - 1][j - 1] = Object.assign({}, objBuffer);
             this.field[i][j] = Object.assign({}, objBuffer);
             this.cleanHints();
             this.cleanChoice();
             this.makeQueen(y_axis, x_axis);
             return;
-          } 
-          else {
+          } else {
             continue;
           }
           // if (moveFlag) {
@@ -370,7 +390,7 @@ export default {
       const x_axis = y.indexOf(x);
       // Analyzing chosen checker's surroundings
       // if checker is black we analyze only top line of chozen analyze block if checker is white we do vice versa
-      if (x.isBlack) {
+      if (x.isBlack && !x.isQueen) {
         for (let i = y_axis - 1; i < y_axis + 2; i++) {
           /* Trick is at this line in i declaration */
           for (let j = x_axis - 1; j < x_axis + 2; j++) {
@@ -389,7 +409,8 @@ export default {
             // four ifs below render hints for killing enemy for black checkers
             // You should read the conditions like this. If analyzed square has one checker and there's chosen opposite checker near and...
             // ... there's blank square right after analyzed square we do hint
-            if (i + 1 <= 7 && i - 1 >= 0 && j + 1 <= 7 && j - 1 >= 0) {  /* by this we escape checking unexisting checkers or chosen checkers */
+            if (i + 1 <= 7 && i - 1 >= 0 && j + 1 <= 7 && j - 1 >= 0) {
+              /* by this we escape checking unexisting checkers or chosen checkers */
               if (
                 this.field[i][j].isWhite &&
                 this.field[i + 1][j + 1].isChosen &&
@@ -421,7 +442,7 @@ export default {
             }
           }
         }
-      } else {
+      } else if (x.isWhite && !x.isQueen) {
         for (let i = y_axis - 1; i < y_axis + 2; i++) {
           /* Trick is at this line in i declaration */
           for (let j = x_axis - 1; j < x_axis + 2; j++) {
@@ -437,7 +458,8 @@ export default {
               this.field[i][j].isHinted = 1;
             }
             // four ifs below render hints for killing enemy for white checkers
-            if (i + 1 <= 7 && i - 1 >= 0 && j + 1 <= 7 && j - 1 >= 0) {  /* by this we escape checking unexisting checkers or chosen checkers */
+            if (i + 1 <= 7 && i - 1 >= 0 && j + 1 <= 7 && j - 1 >= 0) {
+              /* by this we escape checking unexisting checkers or chosen checkers */
               if (
                 this.field[i][j].isBlack &&
                 this.field[i + 1][j + 1].isChosen &&
@@ -469,11 +491,25 @@ export default {
             }
           }
         }
+      } else if (x.isBlack && x.isQueen) {
+        this.renderQueenHints(y_axis, x_axis);
+      } else if (x.isWhite && x.isQueen) {
+        this.renderQueenHints(y_axis, x_axis);
+      }
+    },
+    renderQueenHints(y_axis, x_axis) {
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          if (i == y_axis && j == x_axis) continue;
+          if (!this.field[i][j].isChecker && this.field[i][j].isPlayble) {
+            this.field[i][j].isHinted = 1;
+          }
+        }
       }
     },
   },
   mounted() {
-    console.log('hey!');
+    console.log("hey!");
     this.createId();
     this.initRender();
     // console.log(this.field);
